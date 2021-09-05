75fb5488bf716f1f39f286d0174ed7c8158a9e55
ghettodimasta <54914863+ghettodimasta@users.noreply.github.com>
2021-09-06 01:34:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ async def delete_file(request):
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes(routes)
-    web.run_app(app, host='127.0.0.1', port=80)
+    web.run_app(app, host='127.0.0.1', port=4040)
